3ad83a97e407db96573e463ae11d93805e6aa48c
alipnf <alipnf@proton.me>
2024-07-01 11:22:24 +0700

    style using bootstrap

--- components/header.php
+++ components/header.php
@@ -1,27 +1,51 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library Management</title>
-    <link rel="stylesheet" href="path/to/your/css/file.css">
+    <title>Manajemen Perpustakaan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <header>
-        <h1>Library Management</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="/Uas-Praktikum-Web/index.php">Manajemen Perpustakaan</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/Uas-Praktikum-Web/index.php">Beranda</a>
+                        </li>
+                    </ul>
+                    <?php if(isset($_SESSION['username'])): ?>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <span class="nav-link">Selamat datang, <?php echo $_SESSION['username']; ?></span>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-danger" href="/Uas-Praktikum-Web/auth/logout.php">Logout</a>
+                            </li>
+                        </ul>
+                    <?php else: ?>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/Uas-Praktikum-Web/auth/login.php">Login</a>
+                            </li>
+                        </ul>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </nav>
     </header>
-    <nav>
-        <ul>
-            <li><a href="/Uas-Praktikum-Web/index.php">Home</a></li>
-            <li><a href="/Uas-Praktikum-Web/books/create.php">Add Book</a></li>
-            <?php if(isset($_SESSION['username'])): ?>
-                <li><a href="/Uas-Praktikum-Web/auth/logout.php">Logout</a></li>
-                <li><span>Welcome, <?php echo $_SESSION['username']; ?></span></li>
-            <?php else: ?>
-                <li><a href="/Uas-Praktikum-Web/auth/login.php">Login</a></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
