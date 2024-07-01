cb7e0233b5b024899fdbd2c981b6d69416f7ce39
alipnf <alipnf@proton.me>
2024-07-01 11:22:22 +0700

    style using bootstrap

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,11 @@
-<footer>
-        <p>&copy; 2024 Library Management</p>
-    </footer>
+<footer class="footer mt-auto py-3 bg-light" style="
+    position: absolute;
+    bottom: 0;
+    width: 100%;">
+    <div class="container text-center">
+        <p>&copy; 2024 Manajemen Perpustakaan</p>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
